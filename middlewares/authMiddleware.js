bcf52e7d13231de45e05684defbe80442ec41fe3
ismail hossain <ismail15-4216@diu.edu.bd>
2025-06-18 00:03:00 +0600

    Initial commit

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const protect = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const token = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
-
-  // ✅ Extract token from "Bearer <token>"
-  const token = authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'No token' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.id;
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: 'Invalid token' });
   }
 };
 
